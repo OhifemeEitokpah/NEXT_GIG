6e3c8817372e4c36bc7857017f2b0992b6efec60
Andre Oliveira <andrehmo@hotmail.com>
2025-05-27 12:11:31 +0200

    Models validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  belongs_to :user
   belongs_to :venue
   has_many :bookings
+  has_many :user through: :bookings
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :venues
-  has_many :events
   has_many :bookings
+  has_many :events through: :bookings
   enum role: { musician: 0, organiser: 1 }
 end
