9f3955a3501341c66e4b4b7217dee0d48a85c402
Andre Oliveira <andrehmo@hotmail.com>
2025-05-29 16:58:22 +0200

    Fixes the seeds 😬

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :venue
-  belongs_to :user # organiser
   has_many :bookings
   has_many :users, through: :bookings # musicians who booked
   has_one_attached :photo
