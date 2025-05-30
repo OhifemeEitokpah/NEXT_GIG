4d4f075454066f14b5f1e492daf25eb93c1ed89a
Andre Oliveira <andrehmo@hotmail.com>
2025-05-30 16:05:06 +0200

    Fixing production

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,21 +4,21 @@ class Notification < ApplicationRecord
 
   scope :unread, -> { where(read_at: nil) }
 
-  after_create_commit do
-    # prepend the new notification into the list
-    broadcast_prepend_to(
-      user,
-      target: "notifications_list",
-      partial: "shared/notification_item",
-      locals: { notification: self }
-    )
+  # after_create_commit do
+  #   # prepend the new notification into the list
+  #   broadcast_prepend_to(
+  #     user,
+  #     target: "notifications_list",
+  #     partial: "shared/notification_item",
+  #     locals: { notification: self }
+  #   )
 
-    # update the badge count
-    broadcast_replace_to(
-      user,
-      target: "notifications_count",
-      partial: "shared/notification_count",
-      locals: { count: user.notifications.unread.count }
-    )
-  end
+  #   # update the badge count
+  #   broadcast_replace_to(
+  #     user,
+  #     target: "notifications_count",
+  #     partial: "shared/notification_count",
+  #     locals: { count: user.notifications.unread.count }
+  #   )
+  # end
 end
